867a0fd3a7f4251065ff041e71c65b73ec20f038
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2025-09-15 04:26:55 +0800

    AGENDA FUNCTIONALITY DONE... KINDA. TABLE NOT IMPLEMENTED YET DUE TO THERE BEING NO PROJECTS YET

--- internal/agenda/urls.py
+++ internal/agenda/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import agenda_view, add_agenda_view
+from .views import agenda_view, add_agenda_view, edit_agenda_view, delete_agenda_view
 
 urlpatterns = [
     path('', agenda_view, name='agenda'),
     path('add/', add_agenda_view, name='add_agenda'),
+    path('edit/<int:agenda_id>/', edit_agenda_view, name='edit_agenda'),
+    path('delete/<int:agenda_id>/', delete_agenda_view, name='delete_agenda'),
 ]
--- internal/agenda/views.py
+++ internal/agenda/views.py
@@ -8,7 +8,8 @@ from django.contrib.auth.decorators import login_required
 @login_required
 @role_required(allowed_roles=["VP", "DIRECTOR"])
 def agenda_view(request):
-    return render(request, 'agenda/agenda.html')
+    agendas = Agenda.objects.prefetch_related('concerned_colleges').all()
+    return render(request, 'agenda/agenda.html', {'agendas': agendas})
 
 
 # Add Agenda View
@@ -24,3 +25,52 @@ def add_agenda_view(request):
         form = AgendaForm()
     return render(request, 'agenda/add_agenda.html', {'form': form})
 from django.shortcuts import render
+
+
+# Edit Agenda View
+@login_required
+@role_required(allowed_roles=["VP", "DIRECTOR"])
+def edit_agenda_view(request, agenda_id):
+    try:
+        agenda = Agenda.objects.get(id=agenda_id)
+    except Agenda.DoesNotExist:
+        return render(request, 'agenda/edit_agenda.html', {'error': 'Agenda not found.'})
+
+    if request.method == 'POST':
+        form = AgendaForm(request.POST, instance=agenda)
+        if form.is_valid():
+            form.save()
+            selected_college_ids = [str(c.id) for c in form.cleaned_data['concerned_colleges']]
+            return render(request, 'agenda/edit_agenda.html', {
+                'form': form,
+                'success': True,
+                'selected_college_ids': selected_college_ids if selected_college_ids else [],
+            })
+        else:
+            # If form is invalid, try to get from POST data
+            selected_college_ids = request.POST.getlist('concerned_colleges')
+    else:
+        form = AgendaForm(instance=agenda)
+        selected_college_ids = [str(c.id) for c in agenda.concerned_colleges.all()] if agenda else []
+    return render(request, 'agenda/edit_agenda.html', {
+        'form': form,
+        'selected_college_ids': selected_college_ids if selected_college_ids else [],
+    })
+
+
+
+# Delete Agenda View
+@login_required
+@role_required(allowed_roles=["VP", "DIRECTOR"])
+def delete_agenda_view(request, agenda_id):
+    try:
+        agenda = Agenda.objects.get(id=agenda_id)
+    except Agenda.DoesNotExist:
+        return render(request, 'agenda/agenda.html', {'agendas': Agenda.objects.all(), 'error': 'Agenda not found.'})
+
+    if request.method == 'POST':
+        agenda.delete()
+        return render(request, 'agenda/agenda.html', {'agendas': Agenda.objects.all(), 'success': 'Agenda deleted successfully.'})
+    else:
+        # Show confirmation dialog
+        return render(request, 'agenda/delete_agenda.html', {'agenda': agenda})
