ae330cf6a2a20c1fa9c4fd8c9a30a62ec904db78
Imee <202280142@psu.palawan.edu.ph>
2025-10-31 19:53:23 +0800

    User filters dropdown fixed -public floatng filter bug

--- system/logs/migrations/0002_initial.py
+++ system/logs/migrations/0002_initial.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('downloadables', '0001_initial'),
+        ('logs', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='downloadable',
-            name='uploaded_by',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='uploaded_downloadables', to=settings.AUTH_USER_MODEL),
+            model_name='logentry',
+            name='user',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='logs_entries', to=settings.AUTH_USER_MODEL),
         ),
     ]
