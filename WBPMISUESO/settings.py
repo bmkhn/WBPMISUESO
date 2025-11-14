c353d69a5d824097afe367f708c141f4ed4aff4f
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2025-11-14 12:58:43 +0800

    Mount Media Setup

--- WBPMISUESO/settings.py
+++ WBPMISUESO/settings.py
@@ -213,7 +213,12 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# For Railway deployment with volume mounted at /media/
+if os.environ.get('DEPLOYED', 'False') == 'True':
+    MEDIA_ROOT = '/media'
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # File upload settings
 # Increase max upload size to 10MB (default is 2.5MB)
@@ -221,8 +226,6 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB in bytes
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB in bytes
 
 # Use MEDIA_ROOT for temp uploads on Railway
-# Railway filesystem is ephemeral by default - for persistent storage, mount a volume at /app/media
-# Guide: https://docs.railway.app/guides/volumes
 if os.environ.get('DEPLOYED', 'False') == 'True':
     FILE_UPLOAD_TEMP_DIR = os.path.join(MEDIA_ROOT, 'temp_uploads')
     # Create temp directory if it doesn't exist
