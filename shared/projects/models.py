2444467acc26ed09d015d0f78d1c93f46f3af0a4
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2025-12-03 23:11:45 +0800

    Quick Adjustments - Fix Expert Display Bug - Removed Invoices Tab (Commented) - Removed Receipt in ProjectExpense Model

--- shared/projects/models.py
+++ shared/projects/models.py
@@ -438,7 +438,7 @@ class ProjectExpense(models.Model):
     reason = models.TextField(blank=True, null=True)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     date_incurred = models.DateField(default=timezone.now)
-    receipt = models.FileField(upload_to=project_expense_upload_to, blank=True, null=True)
+    # receipt = models.FileField(upload_to=project_expense_upload_to, blank=True, null=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='created_expenses')
     created_at = models.DateTimeField(auto_now_add=True)
 
