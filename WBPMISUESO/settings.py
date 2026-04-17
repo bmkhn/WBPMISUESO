ff8ea98365153866ba8e1ec951d79c9ddd65254f
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2026-04-17 23:50:22 +0800

    Improves dotenv loading to use project root .env file

--- WBPMISUESO/settings.py
+++ WBPMISUESO/settings.py
@@ -12,7 +12,8 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
-load_dotenv()
+import os
+load_dotenv(os.path.join(Path(__file__).resolve().parent.parent, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
